fcdad7fb6a9fa406f77726e110198f4a38b88b65
Stone-16 <918736087@qq.com>
2021-10-23 17:20:26 +0800

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'myst_parser',
+    'recommonmark',
 ]
 
 source_suffix = ['.rst', '.md']
